8acb4c98539cd9d76638ac0b19eef1a387e40ce5
Dilip Woad <dilipwoad777@gmail.com>
2025-08-12 18:49:13 +0530

    chore(backend) : setting up backend request handling configurations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export const app = express();
+
+app.use(cors({
+    origin:'*',
+    credentials:true
+}))
+
+//for request coming in json(format)
+app.use(express.json({limit:"20kb"}));
+
+//for request coming in url%20formart%20 
+//to read by backend easily
+app.use(express.urlencoded({limit:"20kb",extended:true}))
+
+//for handling static data->img,videos,pdf,etc
+app.use(express.static)
+
+//use for req parsing cookies to the backend
+app.use(cookieParser())
